c60433cf3c04cc1ff3ce75ba9af7205b6ea6e0f9
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-09-28 13:56:59 +0000

    Implementação de exibição de tema

--- app/components/Estatisticas.js
+++ app/components/Estatisticas.js
@@ -3,9 +3,13 @@ export function Estatisticas({ tentativasErradas, maxTentativas }) {
     <div className="text-center">
       <div className="text-sm text-stone-600 mb-2">
         Tentativas restantes:
-        <span className="ml-2 font-bold">
+        <span className="ml-1 font-bold">
           {maxTentativas - tentativasErradas}
         </span>
+      </div>
+      <div className="text-sm text-stone-600 mb-2">
+        Tema:
+        <span className="ml-1 font-bold">PROGRAMAÇÃO</span>
       </div>
     </div>
   );
